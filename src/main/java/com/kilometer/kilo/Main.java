d24547b737a4a434196db5ff1c946d57d3993bb1
hartou13 <hartrand13@gmail.com>
2023-01-20 11:34:28 +0300

    Create Main.java

---
+++ src/main/java/com/kilometer/kilo/Main.java
@@ -0,0 +1,15 @@
+package com.kilometer.kilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication(scanBasePackages={"controller","com.kilometer.kilo"})
+@CrossOrigin(origins={"http://localhost:3001/","http://localhost:3000/","http://localhost:80/kilometrage/*"})
+public class Main {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KiloApplication.class, args);
+	}
+	
+}
