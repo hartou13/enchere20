265d67c65a560d8be79f3e4b15468694ff15f1b4
hartou13 <hartrand13@gmail.com>
2023-01-20 13:01:54 +0300

    update

--- src/main/java/connexion/Connexion.java
+++ src/main/java/connexion/Connexion.java
@@ -1,17 +1,27 @@
 package connexion;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import gdao.genericdao.GenericDAO;
+import gdao.genericdao.exception.DatabaseConfException;
 public class Connexion {
     public static Connection getConnexion () {
         Connection conn=null;
           try {
             Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/encheres5","postgres","root");
+            conn = GenericDAO.getConPost();       
           } catch (ClassNotFoundException e) {
             e.printStackTrace();
           }
            catch (SQLException e) {
+            e.printStackTrace();
+          } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          } catch (DatabaseConfException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
           }
           return conn;  
